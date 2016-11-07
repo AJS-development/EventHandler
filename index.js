2868548a182f2a69e5837a579da3cf566aaa6e93
Andrew S <andrews54757@gmail.com>
2016-11-07 10:23:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,28 @@
 module.exports = class EventHandler {
  constructor() {
    this.events = {};
+  this.saved = [];
  }
  save() {
-  
+  var cloned = {}
+  for (var i in this.events) cloned[i] = this.events[i]
+  this.saved.push(cloned)
  }
  restore() {
+  this.events = this.saved.pop()
   
  }
  clearAll() {
-  
+  this.events = {};
  }
  events() {
-  
+  var a = []
+  for (var i in this.events) a.push(i)
+   return a
+   
  }
   on(event,call) {
-  
+  this.events[event] = call;
   }
   
 }
