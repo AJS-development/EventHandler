c0c4ac24711a3c9cca524d06d19fe1f2092f3f07
Andrew S <andrews54757@gmail.com>
2016-11-07 10:20:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 "use strict"
 module.exports = class EventHandler {
  constructor() {
-   
+   this.events = {};
  }
+ save() {
   
+ }
+ restore() {
+  
+ }
+ clearAll() {
+  
+ }
+ events() {
+  
+ }
+  on(event,call) {
+  
+  }
   
 }
